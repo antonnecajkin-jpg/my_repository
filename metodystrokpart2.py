83f1b25c688e7cc7a5802bb10baa27e603a8385c
Anton Necajkin <antonnecajkin@gmail.com>
2025-11-20 17:45:03 +0300

    в процессе решения

--- metodystrokpart2.py
+++ metodystrokpart2.py
@@ -1,22 +1,30 @@
-class Often_char:
+class First_and_last_occurrence:
     def __init__(self, input_line: str):
         self.input_line = input_line
+        self.find_char = 'f'
         
         
 
-    def find_max_count(self):
-        maxi = ''
-        counter = 0
-        for char in self.input_line:
-            if self.input_line.count(char) >= counter:
-                counter = self.input_line.count(char)
-                maxi = char
+    def find_index_char(self):
+        first_char_in = 0
+        last_char_in = 0
+        if self.find_char not in self.input_line:
+            return 'NO'
+        else:
+             first_char_in = self.input_line.find(self.find_char)
+             for_rfind = self.input_line.replace(self.input_line[0:first_char_in], '_'*first_char_in) 
+             last_char_in = for_rfind.rfind(self.find_char)
+             if last_char_in != 1:
+                 last_char_in = last_char_in
+             return first_char_in, last_char_in
+             
+                 
+                 
+                 
+        
 
 
-        return maxi
 
-
-
-result = Often_char(input()).find_max_count()
+result = First_and_last_occurrence(input()).find_index_char()
 print(result)
 
